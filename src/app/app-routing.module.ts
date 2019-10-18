e74a4197175931a60d2d3cd5e05b580599520ff4
Stephen Hack <stephen.hack@gmail.com>
2019-10-18 12:38:55 -0400

    added angular router with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactFormComponent } from 'src/app/components/contact-form/contact-form.component';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { StoreComponent } from 'src/app/components/store/store.component';
+import { VideoListComponent } from 'src/app/components/video-list/video-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'videos', component: VideoListComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'contact', component: ContactFormComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { StoreComponent } from './components/store/store.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    VideoListComponent,
+    HomeComponent,
+    StoreComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
