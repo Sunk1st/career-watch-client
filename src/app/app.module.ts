8ccd19d8737dd4d0381d2e465bb4bc8b35aa627e
Stephen Hack <stephen.hack@gmail.com>
2019-10-25 13:28:34 -0400

    added spinner to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -33,7 +35,8 @@ import { SafePipe } from './pipes/safe/safe.pipe';
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
